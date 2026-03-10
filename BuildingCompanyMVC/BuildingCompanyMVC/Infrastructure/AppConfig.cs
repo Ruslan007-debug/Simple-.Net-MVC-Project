9318b2e272f13cd5c13f5d1e530adf7bed9e0b16
Ruslan Terpak <ruxa.terpak@gmail.com>
2026-03-11 00:41:08 +0200

    made base architecture and managed base view for main page(not still ready)

---
+++ BuildingCompanyMVC/BuildingCompanyMVC/Infrastructure/AppConfig.cs
@@ -0,0 +1,22 @@
+﻿namespace BuildingCompanyMVC.Infrastructure
+{
+    public class AppConfig
+    {
+        public TinyMce TinyMCE { get; set; } = new TinyMce();
+
+        public Company Company { get; set; } = new Company();
+    }
+
+    public class TinyMce
+    {
+        public string? APIKey { get; set; }
+    }
+
+    public class Company
+    {
+        public string? CompanyName { get; set; }
+        public string? CompanyPhone { get; set; }
+        public string? CompanyPhoneShort { get; set; }
+        public string? CompanyEmail { get; set; }
+    }
+}
