74c08ca317c9a636e9a3ebe6e0dbec2b909f8513
Ruslan Terpak <ruxa.terpak@gmail.com>
2026-03-12 00:47:30 +0200

    made some changes for main page in views, managed Program.cs and bundled&mini files

--- BuildingCompanyMVC/BuildingCompanyMVC/Program.cs
+++ BuildingCompanyMVC/BuildingCompanyMVC/Program.cs
@@ -1,15 +1,34 @@
+using BuildingCompanyMVC.Infrastructure;
+
 namespace BuildingCompanyMVC
 {
-    public class Program
+    public static class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-            app.MapGet("/", () => "Hello World!");
+            IConfigurationBuilder configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(builder.Environment.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
 
-            app.Run();
+            IConfiguration configuration = configurationBuilder.Build();
+            AppConfig appConfig = configuration.GetSection("Project").Get<AppConfig>()!;
+
+            builder.Services.AddControllersWithViews();
+
+            WebApplication app = builder.Build(); 
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            await app.RunAsync();
         }
     }
 }
